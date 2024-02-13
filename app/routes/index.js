7153f6359375c7708f853ee9cbb1f06202536a9a
Gustavo <sasaeucomvoce@hotmail.com>
2024-02-13 14:35:51 -0300

    feat: file upload with formidable

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var formidable = require('formidable');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,7 +9,18 @@ router.get('/', function(req, res, next) {
 
 router.post('/upload', (req, res) => {
 
-  res.json(req.body);
+  let form = new formidable.IncomingForm({
+    uploadDir: './upload',
+    keepExtensions: true
+  });
+
+  form.parse(req, (err, fields, files)=>{
+
+    res.json({
+      files
+    });
+
+  });
 
 });
 
