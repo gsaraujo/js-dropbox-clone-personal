a733d81be40574c1d7fdf7282d85de8593a71177
Gustavo <sasaeucomvoce@hotmail.com>
2024-02-26 20:21:53 -0300

    feat: renaming files on the screen and on firebase

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -46,6 +46,20 @@ class DropBoxController {
 
     initEvents(){
 
+		this.btnRename.addEventListener('click', e=>{
+
+			let li = this.getSelection()[0];
+			let file = JSON.parse(li.dataset.file);
+
+			let name = prompt("Renomear o arquivo:", file.name);
+
+			if(name) {
+				file.name = name;
+				this.getFirebaseRef().child(li.dataset.key).set(file);
+			}
+
+		});
+
 		this.listFilesEl.addEventListener('selectionchange', e=>{
 
 			switch (this.getSelection().length) {
@@ -384,6 +398,7 @@ class DropBoxController {
       let li = document.createElement('li');
 
       li.dataset.key = key;
+      li.dataset.file = JSON.stringify(file);
 
       li.innerHTML = `
         ${this.getFileIconView(file)}
